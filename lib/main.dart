cbf553de0ac8d725ce61e8588265477b194da0e3
uuttff8 <sassanid18gmail.com>
2019-07-17 17:17:58 +0300

    first try

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:sagrado_flutter_ui/src/screens/chose_place/chose_place_provider.dart';
+import 'src/screens/chose_place/chose_place.dart';
+
+void main() => runApp(Sagrado());
+
+class Sagrado extends StatelessWidget {
+  const Sagrado({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformApp(
+      home: ChangeNotifierProvider<PlaceNotifier>(builder: (_) => PlaceNotifier(), child: ChosePlaceScreen()),
+    );
+  }
+}
---
+++ lib/src/screens/chose_place/chose_place.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:sagrado_flutter_ui/src/screens/chose_place/chose_place_provider.dart';
+
+class ChosePlaceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    PlaceNotifier placesState = Provider.of<PlaceNotifier>(context);
+
+
+    final Size size = MediaQuery.of(context).size;
+    final double itemHeight = 197;
+    final double itemWidth = size.width / 2;
+
+    return PlatformScaffold(
+      appBar: PlatformAppBar(
+        leading: PlatformButton(
+          child: Text('Выбрать все'),
+          ios: (_) {
+            return CupertinoButtonData(padding: EdgeInsets.zero);
+          },
+          onPressed: () {},
+        ),
+        trailingActions: <Widget>[
+          PlatformButton(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Далее'),
+                Icon(Icons.chevron_right),
+              ],
+            ),
+            ios: (_) {
+              return CupertinoButtonData(padding: EdgeInsets.zero);
+            },
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: new Container(
+        child: new GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: (itemWidth / itemHeight),
+          controller: new ScrollController(keepScrollOffset: false),
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          children: List.generate(
+            placesState.places.length,
+            (index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: FlatButton(
+                  child: Column(
+                    children: <Widget>[
+                      Stack(
+                        alignment: Alignment.topRight,
+                        children: [
+                          Opacity(
+                            opacity: placesState.places[index].isSelected ? 0.6 : 1.0,
+                            child: Image.network(
+                              placesState.places[index].imageUrl,
+                              fit: BoxFit.fill,
+                              width: 167,
+                              height: 167,
+                            ),
+                          ),
+                          Image.asset(
+                            placesState.places[index].isSelected
+                                ? 'assets/images/check-active.png'
+                                : 'assets/images/check-inactive.png',
+                            width: 24 / 1.2,
+                            height: 24 / 1.2,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(placesState.places[index].title),
+                        ],
+                      )
+                    ],
+                  ),
+                  onPressed: () {
+                    placesState.onClick(placesState.places[index]);
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
